6d87c70b168ef12deb25e903caeffa48d2f5047a
ocPaul <stranz190097@sr.htlweiz.at>
2023-05-02 10:58:23 +0200

    documented

--- src/main.cpp
+++ src/main.cpp
@@ -5,13 +5,17 @@
 #include <cstring>
 #include <PubSubClient.h>
 
+// pin for analog read
 const int voltInput = 34;
-float voltage;
-int analogValue = 0;
 
+float voltage;
+int analogValue;
+
+// wlan config
 const char* ssid = "THAMONKEE";
 const char* password = "Monkeeee";
 
+// ip address of the mqtt-broker
 const char* mqtt_server = "172.31.200.150";
 
 WiFiClient espClient;
@@ -20,10 +24,10 @@ long lastMsg = 0;
 char msg[50];
 int value = 0;
 
-String message ="ö";
-char* arry;
+String output;
 
 void setup_wifi() {
+  // connecting to the wifi
   delay(10);
   Serial.println();
   Serial.print("Connecting to ");
@@ -42,34 +46,19 @@ void setup_wifi() {
   Serial.println(WiFi.localIP());
 }
 
-void callback(char* topic, byte* message, unsigned int length) {
-  Serial.print("Value arrived on topic: ");
-  Serial.print(topic);
-  Serial.print(". Value: ");
-  String valueTemp;
-  
-  for (int i = 0; i < length; i++) {
-    Serial.print((char)message[i]);
-    valueTemp += (char)message[i];
-  }
-  // Serial.print(messageTemp);
-  Serial.println();
-  voltage = valueTemp.toFloat();
-}
-
 void reconnect() {
   // Loop until we're reconnected
   while (!client.connected()) {
     Serial.print("Attempting MQTT connection...");
+    
     // Attempt to connect
     if (client.connect("ESP8266Client")) {
       Serial.println("connected");
-      // Subscribe
-      client.subscribe("esp32/output");
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
       Serial.println(" try again in 5 seconds");
+      
       // Wait 5 seconds before retrying
       delay(5000);
     }
@@ -77,21 +66,30 @@ void reconnect() {
 }
 
 void setup() {
+  // start serial monitor
   Serial.begin(115200);
+  
   setup_wifi();
+
+  // set values of mqtt Server
   client.setServer(mqtt_server, 1883);
-  client.setCallback(callback);
   }
 
 void loop() {
+  // reconnect mqtt if disconnected
   if (!client.connected()) {
    reconnect();
   }
+
+  // read voltage + map to true value
   analogValue = analogRead(voltInput);
   voltage = analogValue / 4095.0 * 9.25;
-  // Serial.println(voltage);
-  message = String(voltage, 1);
-  client.publish("voltage", message.c_str());
+
+  // convert float type to String with one decimal point 
+  output = String(voltage, 1);
+  
+  // publish value on voltage
+  client.publish("voltage", output.c_str());
 
   delay(1000);
 }
